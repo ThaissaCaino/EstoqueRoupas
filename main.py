18e82fda71212292e8afda847f284b541d840111
ThaissaCaino <almeida.c.thaissa@gmail.com>
2024-06-27 20:34:10 -0300

    consulta

--- main.py
+++ main.py
@@ -43,13 +43,14 @@ def index():
 def consulta():
     cursor.execute('''SELECT * FROM roupas''')
     listar_roupas = cursor.fetchall()
-    
+
     cursor.execute('''SELECT * FROM fornecedor''')
     listar_fornecedor = cursor.fetchall()
     return render_template('consulta.html',
                             listar_roupas=listar_roupas, 
                             listar_fornecedor=listar_fornecedor, 
-                            titulo="Lista das roupas e fornecedores") #usar listar_fornecedor e listar_roupas no consulta.html 
+                            titulo="Lista das roupas e fornecedores") 
+#usar listar_fornecedor e listar_roupas no consulta.html 
 
 if __name__ == '__main__':
     app.run(debug=True)
